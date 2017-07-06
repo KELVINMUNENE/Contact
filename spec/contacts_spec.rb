56c08d8868024bc7979ea2da003ff4ed0081ac2e
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-06 09:06:19 +0300

    described contact

--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -1,2 +1,17 @@
 require('rspec')
   require('contacts')
+
+
+describe(Contacts) do
+  describe("#description") do
+    it("lets you give contacts") do
+      test_contact = Contact.new("Lydia Wairimu - 0701553924")
+      expect(test_contact.description()).to(eq("Lydia Wairimu -0701553924"))
+    end
+  end
+end
+
+
+
+
+  
