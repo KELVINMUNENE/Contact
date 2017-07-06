0c4c917d65adbbe44b6f4208c8f4103645a3dd69
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-06 11:59:37 +0300

    added a class variable to store last name + email method

--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -1,17 +1,46 @@
 require('rspec')
   require('contacts')
 
+  describe('Contacts') do
+    before() do
+      Contact.clear()
+    end
 
-describe(Contacts) do
   describe("#description") do
     it("lets you give contacts") do
       test_contact = Contact.new("Lydia Wairimu - 0701553924")
       expect(test_contact.description()).to(eq("Lydia Wairimu -0701553924"))
     end
   end
+
+
+
+      describe("#first_name") do
+        it("returns the first name of the person") do
+          test_contact = Contact.new("Chuck", "Norris")
+          expect(test_contact.first_name()).to(eq("Chuck"))
+        end
+      end
+
+
+          describe("#last_name") do
+            it("returns the last name of the person") do
+              test_contact = Contact.new("Carmello", "Antony")
+              expect(test_contact.last_name()).to(eq("Antony"))
+            end
+          end
+
+
+
+              describe("#email") do
+                it("returns the email of the person") do
+                  test_contact = Contact.new("uhurukenyatta@gmail.com")
+                  expect(test_contact.email()).to(eq("uhurukenyatta@gmail.com"))
+                end
+              end
+
+      
+
+
+
 end
-
-
-
-
-  
