57638de8a05d915384092a3fec863c8468ef60ca
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-06 14:13:03 +0300

    added a file

--- app.rb
+++ app.rb
@@ -11,3 +11,17 @@ end
 get('/contacts/new') do
   erb(:contact_form)
 end
+
+
+
+post('/contacts') do
+    first_name = params.fetch('first_name')
+    last_name = params.fetch('last_name')
+    email = params.fetch('email')
+    company = params.fetch('company')
+    job_title = params.fetch('job_title')
+    phone_number = parasm.fetch('phone_number')
+    @contact = Contact.new(first_name,last_name, email, company, job_title, phone_number)
+    @contact.save()
+    erb(:added)
+  end
